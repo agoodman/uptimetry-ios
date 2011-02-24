a874b1412755e544185ed13b8ac008961bfe2a44
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-02-24 17:32:41 -0500

    against my better judgement, adding OR not as a submodule

---
+++ objective_resource/NSMutableURLRequest+ResponseType.m
@@ -0,0 +1,33 @@
+//
+//  NSMutableURLRequest+ResponseType.m
+//  active_resource
+//
+//  Created by James Burka on 1/19/09.
+//  Copyright 2009 Burkaprojects. All rights reserved.
+//
+
+#import "NSMutableURLRequest+ResponseType.h"
+#import "ObjectiveResource.h"
+#import "ORConnection.h"
+
+@implementation NSMutableURLRequest(ResponseType)
+
++(NSMutableURLRequest *) requestWithUrl:(NSURL *)url andMethod:(NSString*)method {
+	NSLog(@"%@",url);
+	NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData
+																											timeoutInterval:[ORConnection timeout]];
+	[request setHTTPMethod:method];
+	switch ([ObjectiveResourceConfig getResponseType]) {
+		case JSONResponse:
+			[request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];	
+			[request addValue:@"application/json" forHTTPHeaderField:@"Accept"];
+			break;
+		default:
+			[request setValue:@"application/xml" forHTTPHeaderField:@"Content-Type"];	
+			[request addValue:@"application/xml" forHTTPHeaderField:@"Accept"];
+			break;
+	}
+	return request;
+}
+
+@end
